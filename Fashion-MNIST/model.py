a02ec73b280f0b5d9ae07c01b53768ed22a6cadf
Nikunj Taneja <32366458+underscoreorcus@users.noreply.github.com>
2019-01-30 23:38:29 +0800

    Update model.py

--- Fashion-MNIST/model.py
+++ Fashion-MNIST/model.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 transform = transforms.Compose([transforms.ToTensor(),
-								transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+				transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_data = datasets.FashionMNIST(root = './data', download = True, train = True, transform = transform)
 test_data = datasets.FashionMNIST(root = './data', download = True, train = False, transform = transform)
